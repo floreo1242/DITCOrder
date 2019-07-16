812ccc2394abe16d0ef8e79c5d515ab4dd01b063
Jinsoo Yoon <floreo1242@gmail.com>
2019-07-16 18:23:35 +0900

    Initial commit

---
+++ app/src/main/java/com/github/floreo1242/ditcorder/TimeSelectActivity.kt
@@ -0,0 +1,12 @@
+package com.github.floreo1242.ditcorder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TimeSelectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_select)
+    }
+}
